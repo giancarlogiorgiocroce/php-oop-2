42db04d1aa4e37c30eb7c1f973ca52375072d393
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-06-20 15:05:51 +0200

    Setup base

---
+++ classes/users/products/Product.php
@@ -0,0 +1,18 @@
+<?php
+
+class Product{
+    private $name;
+    private $price;
+    private $barcode;
+
+    public function __construct($_name,
+                                $_price,
+                                $_barcode){
+        $this->name = ;
+        $this->price = ;
+        $this->barcode = ;
+    }
+
+    public function set
+
+}
